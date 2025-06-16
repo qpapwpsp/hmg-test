5d15a86c556eec022a3669753e49aa28aac295f1
Jinyoung Ahn <qpapwpsp@naver.com>
2025-06-16 13:58:10 +0900

    first commit

---
+++ src/main/java/com/hmg/as/test/hmg_test/HmgTestApplication.java
@@ -0,0 +1,13 @@
+package com.hmg.as.test.hmg_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmgTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmgTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmg/as/test/hmg_test/HmgTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmg.as.test.hmg_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmgTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
