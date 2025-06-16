ae560042871d92bd4a651d692c464dbf30d750f3
Jinyoung Ahn <qpapwpsp@naver.com>
2025-06-17 08:56:00 +0900

    FEATURE/AJY_01

--- src/main/java/com/hmg/as/test/hmg_test/HmgTestApplication.java
+++ src/main/java/com/hmg/as/test/hmg_test/HmgTestApplication.java
@@ -3,11 +3,37 @@ package com.hmg.as.test.hmg_test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.hmg.as.test.hmg_test.entity.EnRoInfo;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
 @SpringBootApplication
 public class HmgTestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HmgTestApplication.class, args);
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hmg-test");
+
+		EntityManager em = emf.createEntityManager();
+		
+		EntityTransaction tx = em.getTransaction();
+
+		tx.begin();
+
+		EnRoInfo roInfo = new EnRoInfo("203000", "202506W0001");
+		
+		em.persist(roInfo);
+
+		tx.commit();
+
+		em.close();
+
+		emf.close();
 	}
 
 }
